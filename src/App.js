0b2b03cd05b90b8a0e071e5584187b609a2e54ed
savankumar chaudhary <pavanchaudhary255@gmail.com>
2025-02-14 12:32:57 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
 
-function App() {
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
   return (
-    <div>
-      <h1>Hello DevOps</h1>
+    <div className="App">
+      <h1>Studen ID: 101494897</h1>
+      <h1>Studen Name: Pavankumar Chaudhary</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Pavankumar
+      <StudentList students={students} />
     </div>
   );
-}
+};
 
 export default App;
